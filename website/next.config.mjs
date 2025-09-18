92d167922a940263cf3492e6301e1d4c17151613
‘’khalilboumelala’’ <mohamedkhalil.boumelala@esprit.tn>
2025-09-18 16:57:20 +0200

    Update next.config.mjs

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -1,14 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
+  output: "export", // pour générer en statique
+  compress: true,
+  webpack: (config) => {
+    // Split en plus petits bundles
+    config.optimization.splitChunks = {
+      chunks: "all",
+      minSize: 20000,
+      maxSize: 250000, // force des chunks plus petits
+    };
+    return config;
   },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-}
+};
 
 export default nextConfig
